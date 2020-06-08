af0f2ba7512e5e7e4042452c58a1894533c5029e
ctownsdinjs <ctownsdinjs@gmail.com>
2020-06-08 13:56:01 -0700

    move server.js into src/

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World CTownsdin");
+  res.send(`Hello World - ${new Date().toString()}`);
 });
 
 app.listen(PORT, HOST);
